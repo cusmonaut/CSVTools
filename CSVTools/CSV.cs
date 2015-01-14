d65567bd6088d995a85679ff11a866a576ae1314
cusmonaut <vodtech@hotmail.com>
2015-01-13 23:02:48 -0600

    Adding files

---
+++ CSVTools/CSV.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace CSVTools
+{
+    public class CSV
+    {
+        public IList<Dictionary<string, string>> Rows
+        {
+            get;
+            private set;
+        }
+
+        public CSV(IList<Dictionary<string, string>> rowList)
+        {
+            Rows = rowList as List<Dictionary<string, string>>;
+        }
+
+        public void AddRow(Dictionary<string, string> row)
+        {
+            Rows.Add(row);
+        }
+
+        public void DeleteRow(Dictionary<string, string> row)
+        {
+            Rows.Remove(row);
+        }
+
+        public void DeleteRowAt(int index)
+        {
+            Rows.RemoveAt(index);
+        }
+    }
+}
---
+++ CSVTools/CSVParser.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSVTools
+{
+    public static class CSVParser
+    {
+        public static CSV ParseCSV(string filePath)
+        {
+            CSV csv = new CSV(new List<Dictionary<string, string>>());
+
+            using (TextFieldParser parser = new TextFieldParser(filePath.ToString()))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                string[] colTitles = null;
+
+                if (!parser.EndOfData)
+                {
+                    colTitles = parser.ReadFields();
+                }
+
+                while (!parser.EndOfData)
+                {
+                    csv.AddRow(ProcessRow(parser.ReadFields(), colTitles));
+                }
+            }
+
+            return csv;
+        }
+
+        internal static Dictionary<string, string> ProcessRow(string[] row, string[] titles)
+        {
+            var tempDictionary = new Dictionary<string, string>();
+
+            for (int i = 0; i < titles.Count(); i++)
+            {
+                tempDictionary.Add(titles[i], row[i]);
+            }
+
+            return tempDictionary;
+        }
+    }
+}
